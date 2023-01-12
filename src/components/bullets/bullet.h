ed2d276cdc18f9cb65399b6c866893e3af07261f
staniekk <jstaniekk@gmail.com>
2023-01-12 15:08:51 +0100

    pistolety v.02

--- src/components/bullets/bullet.h
+++ src/components/bullets/bullet.h
@@ -16,19 +16,16 @@ class Bullet : public Component
 public:
     //static unsigned int bullet_ID;
     float facing_angle, delta_x, delta_y;
+	Entity entity_hit;
     Vec2 starting_pos, destination;
     Collider* collider;
     Gun* gun;
     Player* player;
 
-	float attack_cooldown;
-	float damage;
-	float bullet_speed;
-	int magazine_capacity;
-	int bullets_left;
-
-	Sound* shot;
-	Sound* on_hit;
+	float attack_cooldown, bullet_damage, bullet_knockback, bullet_speed;
+	int magazine_capacity, bullets_left;
+	Sound* audio_shot;
+	Sound* audio_on_hit;
 	String weapon_sprite;
 	String bullet_sprite;
 
@@ -54,11 +51,13 @@ public:
 			attack_cooldown = 1.4f * 60;
 			magazine_capacity = 4;
 			bullets_left = magazine_capacity;
-			damage = 21;
+			bullet_damage = 21;
+			bullet_knockback = 0.09f;
 			bullet_speed = 6.2f;
 			weapon_sprite = "icon_shotgun.png";
 			bullet_sprite = "bullet1.png";
-
+			audio_shot = Asset::load_sound("audio/gun_pistol_shot.mp3");
+			audio_on_hit = Asset::load_sound("audio/gun_pistol_on_hit.mp3");
 			break;
 		}
 
@@ -67,10 +66,13 @@ public:
 			attack_cooldown = 0.4f * 60;
 			magazine_capacity = 9;
 			bullets_left = magazine_capacity;
-			damage = 13;
+			bullet_damage = 13;
+			bullet_knockback = 0.02f;
 			bullet_speed = 6.2f;
 			weapon_sprite = "icon_pistol.png";
 			bullet_sprite = "bullet2.png";
+			audio_shot = Asset::load_sound("audio/gun_pistol_shot.mp3");
+			audio_on_hit = Asset::load_sound("audio/gun_pistol_on_hit.mp3");
 
 			break;
 		}
@@ -80,10 +82,13 @@ public:
 			attack_cooldown = 0.15f * 60;
 			magazine_capacity = 60;
 			bullets_left = magazine_capacity;
-			damage = 7;
+			bullet_damage = 7;
+			bullet_knockback = 0.01f;
 			bullet_speed = 4.6f;
 			weapon_sprite = "icon_machine_gun.png";
 			bullet_sprite = "bullet3.png";
+			audio_shot = Asset::load_sound("audio/gun_pistol_on_hit2.mp3");
+			audio_on_hit = Asset::load_sound("audio/gun_pistol_on_hit.mp3");
 
 			break;
 		}
@@ -93,10 +98,13 @@ public:
 			attack_cooldown = 2.5f * 60;
 			magazine_capacity = 3;
 			bullets_left = magazine_capacity;
-			damage = 83;
+			bullet_damage = 83;
+			bullet_knockback = 0.2f;
 			bullet_speed = 8.4f;
 			weapon_sprite = "icon_crossbow.png";
 			bullet_sprite = "bullet4.png";
+			audio_shot = Asset::load_sound("audio/gun_pistol_shot.mp3");
+			audio_on_hit = Asset::load_sound("audio/gun_pistol_on_hit.mp3");
 
 			break;
 		}
@@ -118,12 +126,28 @@ public:
         collider = entity->get<Collider>();
         collider->size = this->entity->get<Sprite>()->tex->size;
         collider->tag = CollisionTag::Bullet;
+
+
+		if (audio_shot!= NULL) {
+			Audio::play_one_shot(audio_shot, 0.05f);
+		}
     }
 
     void update() override
     {
         entity->position.x += cos(facing_angle) * bullet_speed;
         entity->position.y += sin(facing_angle) * bullet_speed;
+
+		// TO DO: offset = w init entity->get<Sprite>()->tex->size.x / 2
+		if (entity->get<Collider>()->check_sphere(Vec2(entity->position.x + entity->get<Sprite>()->tex->size.x / 2, entity->position.y + entity->get<Sprite>()->tex->size.y / 2), 3.0f, CollisionTag::Enemy, entity_hit))
+		{
+			entity_hit.get<Enemy>()->take_damage(bullet_damage, bullet_knockback);
+			if (audio_shot != NULL) {
+				Audio::play_one_shot(audio_on_hit, 0.05f);
+			}
+
+			this->entity->world->remove(this->entity);
+		}
 
     }
 
