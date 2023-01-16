7d4b8a78793027f1c68f95bfb42b373f1ea8c1bc
kubs <mctest2334@gmail.com>
2023-01-16 21:24:32 +0100

    cleanup

--- src/tools/tool_anim.h
+++ src/tools/tool_anim.h
@@ -1,76 +1,10 @@
 #pragma once
 
 #include "tool.h"
-#include <components/animator.h>
-
 #include <core/types.h>
 
-#include <cereal/types/unordered_map.hpp>
-#include <cereal/types/vector.hpp>
-#include <cereal/types/memory.hpp>
-#include <cereal/archives/binary.hpp>
+#include <core/file/atl.h>
 
-#include <lib/igfiledialog/ImGuiFileDialog.h>
-#include <lib/json.hpp>
-#include <cereal/archives/json.hpp>
-
-namespace glm
-{
-
-	template<class Archive> void serialize(Archive& archive, glm::vec2& v) { archive(v.x, v.y); }
-	template<class Archive> void serialize(Archive& archive, glm::vec3& v) { archive(v.x, v.y, v.z); }
-	template<class Archive> void serialize(Archive& archive, glm::vec4& v) { archive(v.x, v.y, v.z, v.w); }
-	template<class Archive> void serialize(Archive& archive, glm::ivec2& v) { archive(v.x, v.y); }
-	template<class Archive> void serialize(Archive& archive, glm::ivec3& v) { archive(v.x, v.y, v.z); }
-	template<class Archive> void serialize(Archive& archive, glm::ivec4& v) { archive(v.x, v.y, v.z, v.w); }
-	template<class Archive> void serialize(Archive& archive, glm::uvec2& v) { archive(v.x, v.y); }
-	template<class Archive> void serialize(Archive& archive, glm::uvec3& v) { archive(v.x, v.y, v.z); }
-	template<class Archive> void serialize(Archive& archive, glm::uvec4& v) { archive(v.x, v.y, v.z, v.w); }
-	template<class Archive> void serialize(Archive& archive, glm::dvec2& v) { archive(v.x, v.y); }
-	template<class Archive> void serialize(Archive& archive, glm::dvec3& v) { archive(v.x, v.y, v.z); }
-	template<class Archive> void serialize(Archive& archive, glm::dvec4& v) { archive(v.x, v.y, v.z, v.w); }
-
-	// glm matrices serialization
-	template<class Archive> void serialize(Archive& archive, glm::mat2& m) { archive(m[0], m[1]); }
-	template<class Archive> void serialize(Archive& archive, glm::dmat2& m) { archive(m[0], m[1]); }
-	template<class Archive> void serialize(Archive& archive, glm::mat3& m) { archive(m[0], m[1], m[2]); }
-	template<class Archive> void serialize(Archive& archive, glm::mat4& m) { archive(m[0], m[1], m[2], m[3]); }
-	template<class Archive> void serialize(Archive& archive, glm::dmat4& m) { archive(m[0], m[1], m[2], m[3]); }
-
-	template<class Archive> void serialize(Archive& archive, glm::quat& q) { archive(q.x, q.y, q.z, q.w); }
-	template<class Archive> void serialize(Archive& archive, glm::dquat& q) { archive(q.x, q.y, q.z, q.w); }
-
-}
-
-struct AtlasSprite {
-	AtlasSprite() : pos(0, 0), size(0, 0) {};
-	AtlasSprite(Vec2 p, Vec2 s) : pos(p), size(s) {};
-
-	Vec2 pos;
-	Vec2 size;
-	// thats all
-
-	template <class Archive>
-	void serialize(Archive& ar)
-	{
-		ar(pos, size);
-	}
-};
-
-struct AtlasData {
-	String name;
-	Vector<char> tex_data;
-	uint32_t tex_data_size;
-
-	Vector<AtlasSprite> sprites;
-	uint32_t sprites_count;
-
-	template <class Archive>
-	void serialize(Archive& ar)
-	{
-		ar(name, tex_data, tex_data_size, sprites, sprites_count);
-	}
-};
 
 class AnimationTool : public Tool {
 
